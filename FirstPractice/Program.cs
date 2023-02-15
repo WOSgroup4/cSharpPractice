455ff0a29779df3aef8276afdfb373770744e860
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-02-15 15:57:59 -0600

    Array and loop through complete (kaw)

--- FirstPractice/Program.cs
+++ FirstPractice/Program.cs
@@ -1,8 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// List and loop through (Ashunti)
+﻿// List and loop through (Ashunti)
 
 // Array and loop through (Katie)
+double[] probabilities = {0.7645, 0.67, 0.882, 0.42, 0.95, 0.23, 0.366};
+
+foreach(double statistic in probabilities){
+    Console.WriteLine($"The likely hood of this happening is: {statistic} or {statistic*100}%");
+}
 
 // Dictionary and loop through (Joslyn)

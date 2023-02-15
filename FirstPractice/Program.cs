12a6409e4e90b6eff8d588bf48553b20b3cc9602
Ashunti Ware <ashunti@Ashuntis-MacBook-Air.local>
2023-02-15 16:14:06 -0600

    List and loop through

--- FirstPractice/Program.cs
+++ FirstPractice/Program.cs
@@ -2,6 +2,17 @@
 Console.WriteLine("Hello, World!");
 
 // List and loop through (Ashunti)
+List<string> makeup = new List <string> ();
+makeup.Add("eyeshadow");
+makeup.Add("foundation");
+makeup.Add("lipstick");
+makeup.Add("blush");
+
+for(int i = 0; i < makeup.Count; i++){
+    Console.WriteLine(makeup[i]);
+}
+
+
 
 // Array and loop through (Katie)
 

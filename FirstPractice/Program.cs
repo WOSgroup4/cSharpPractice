addb8201dba5920c627616a7614974d3ad998689
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-02-15 15:30:24 -0600

    Added roles for FirstProject

--- FirstPractice/Program.cs
+++ FirstPractice/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// List and loop through (Ashunti)
+
+// Array and loop through (Katie)
+
+// Dictionary and loop through (Joslyn)
